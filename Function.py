2aac78dd927379042cea08f3be800aedc6f9ae6e
Vishnu Medhini <vishnumedhini@gmail.com>
2026-09-16 21:10:38 +0530

    Functions

---
+++ Function.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+arr1 = np.zeros((4,2))
+print(arr1)
+
+arr2 = np.ones((3,4))
+print(arr2)
+
+arr3 = np.full(2,4)
+print(arr3)
+
+arr4 = np.full((2,3),45)
+print(arr4)
+
+arr5 = np.eye(5)
+print(arr5)
