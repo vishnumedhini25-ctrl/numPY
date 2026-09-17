d1234dfa8cceca735281c229750ff758cbebf771
Vishnu Medhini <vishnumedhini@gmail.com>
2026-09-17 20:55:42 +0530

    Functions

--- Function.py
+++ Function.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 arr1 = np.zeros((4,2))
 print(arr1)
 
@@ -15,3 +14,22 @@ print(arr4)
 
 arr5 = np.eye(5)
 print(arr5)
+
+arr6 =np.arange(0,100,2)
+print(arr6)
+
+arr7 = np.linspace(0,100,5)
+print(arr7)
+
+print(np.sum(arr6))
+
+print(np.min(arr6))
+print(np.max(arr6))
+print(np.median(arr6))
+print(np.std(arr6))
+
+arr8 = np.array([[1,2,3],
+                 [4,5,6]])
+print(np.sum(arr8))
+print(np.sum(arr8,axis=0))
+print(np.sum(arr8,axis=1))
